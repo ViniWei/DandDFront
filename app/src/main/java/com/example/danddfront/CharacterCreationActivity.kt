013acd3fc96cc8d469329050e878c34519d83c82
ViniWei <viniciusweigert152@gmail.com>
2024-10-13 15:22:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/danddfront/CharacterCreationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.danddfront
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CharacterCreationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_creation)
+    }
+}
---
+++ app/src/main/java/com/example/danddfront/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.danddfront
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val characterButton : Button = findViewById<Button>(R.id.CreateCharacterMenuButton)
+        characterButton.setOnClickListener {
+            val intent = Intent(this, CharacterCreationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
